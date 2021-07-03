00b519b1bcad05db330481247219ea7bc954d478
Hanan <ensw03217@student.su.edu.krd>
2021-07-03 17:34:37 +0300

    saving data in local storage

--- src/store.js
+++ src/store.js
@@ -10,9 +10,39 @@ const Reducers = combineReducers({
   Todos: ListTodosReducer,
 });
 
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('todos');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('todos', serializedState);
+  } catch {
+    // ignore write errors
+  }
+};
+
+const persistedState = loadState();
+
 const Store = createStore(
   Reducers,
-  intialState,
+  persistedState,
   composeWithDevTools(applyMiddleware(...middilware))
 );
+
+Store.subscribe(() => {
+  saveState({
+    Todos: Store.getState().Todos,
+  });
+});
+
 export default Store;
